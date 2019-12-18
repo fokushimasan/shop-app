a6b5c878226386c70b6f3e1f8c9e0c08b3e49b8c
Welliton Klein <welliton.fokushima@gmail.com>
2019-12-18 11:41:08 -0200

    Mostrando caixa de dialogo de alerta

--- lib/widgeets/cart_item.dart
+++ lib/widgeets/cart_item.dart
@@ -37,6 +37,26 @@ class CartItem extends StatelessWidget {
         ),
       ),
       direction: DismissDirection.endToStart,
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: const Text('Are you sure?'),
+            content:
+                const Text('Do you want to remove the item from the cart?'),
+            actions: <Widget>[
+              FlatButton(
+                child: const Text('No'),
+                onPressed: () => Navigator.of(ctx).pop(false),
+              ),
+              FlatButton(
+                child: const Text('Yes'),
+                onPressed: () => Navigator.of(ctx).pop(true),
+              ),
+            ],
+          ),
+        );
+      },
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },

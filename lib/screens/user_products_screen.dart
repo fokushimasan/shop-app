02229f4235115ab44da7e3036fc06520e39691c1
Welliton Klein <welliton.fokushima@gmail.com>
2019-12-18 12:15:19 -0200

    Adicionando uma pagina 'Gerenciar Produtos'

---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
+import '../widgeets/user_product_item.dart';
+import '../widgeets/app_drawer.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Products'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              // ...
+            },
+          )
+        ],
+      ),
+      drawer: AppDrawer(),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: ListView.builder(
+          itemCount: productsData.items.length,
+          itemBuilder: (_, i) => Column(
+            children: [
+              UserProductItem(
+                title: productsData.items[i].title,
+                imageUrl: productsData.items[i].imageUrl,
+              ),
+              const Divider(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
